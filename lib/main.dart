6af122a1ae743283f8504afef159b92e45fd959e
Paulo Belo <pjbelo@gmail.com>
2020-10-02 16:58:27 +0100

    delete comented lines

--- lib/main.dart
+++ lib/main.dart
@@ -8,21 +8,6 @@ class Todo {
   Todo(this.title, this.description);
 }
 
-//void main() {
-//  runApp(MaterialApp(
-//    title: 'Passing Data',
-//    home: TodosScreen(
-//      todos: List.generate(
-//        20,
-//        (i) => Todo(
-//          'Todo $i',
-//          'A description of what needs to be done for Todo $i',
-//        ),
-//      ),
-//    ),
-//  ));
-//}
-
 void main() {
   runApp(MyApp());
 }
